c77642e94e61a672e72b112c19127a7a610afc0d
SuhoRyu <88270452+SuhoRyu@users.noreply.github.com>
2025-06-03 11:05:42 +0900

    Add files via upload

---
+++ utils/t2v_metrics/__init__.py
@@ -0,0 +1,16 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .constants import HF_CACHE_DIR
+from .vqascore import VQAScore, list_all_vqascore_models
+
+def list_all_models():
+    return list_all_vqascore_models()
+
+def get_score_model(model='clip-flant5-xxl', device='cuda', cache_dir=HF_CACHE_DIR, **kwargs):
+    if model in list_all_vqascore_models():
+        return VQAScore(model, device=device, cache_dir=cache_dir, **kwargs)
+    else:
+        raise NotImplementedError()
